ea35f71b60321054b84b5e99147d903a77ee0be1
Ojeriakhi <joyojeey@gmail.com>
2025-02-17 12:36:03 +0100

    i made changes

--- app.vue
+++ app.vue
@@ -1,5 +1,182 @@
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <template>
+    <section class="px-28 py-4 flex justify-center flex-col align-center ">
+     <header class="flex justify-between">
+         <div>
+             <img src="public/images/logo.png" alt="">
+         </div>
+ 
+         <ul class="flex gap-6 text-[#475467]">
+             <li class="p-1">Home</li>
+             <li class="p-1">Products</li>
+             <li class="p-1">Resources</li>
+             <li class="p-1">Pricing</li>
+         </ul>
+         <div class="">
+             <button class="bg-[#007AFF] text-white rounded-xl px-3 py-1 ">Contact Us</button>
+         </div>
+     </header>
+ 
+     <section class="mt-[100px] flex justify-between">
+         <div>
+             <h1 class="text-[#101828] text-6xl font-semibold">Empowering Businesses <br> with IT Solutions.</h1>
+             <p class="text-[#475467] mt-6">At   , we specialize in software  <br> development, IT supplies, and consultancy, <br> delivering tailored solutions that drive growth and <br>innovation.</p>
+ 
+ 
+             <div class="mt-10">
+                 <img src="public/images/userreviews.png" alt="">
+ 
+             </div>
+         
+         </div>
+ 
+         <div>
+             <h3 class="text-[#101828] text-3xl font-semibold text-center ">Contact us Today</h3>
+             <span class="mt-2 text-[#475467] text-center flex justify-center">Looking to build something?</span>
+             <form action="" class=" mt-3">
+                 <label class="block mt-4 text-[#344054] text-sm">Name*</label>
+                 <input type="password" placeholder="Enter your name" class="border border-gray-300 rounded rounded-2xl w-[400px]  p-2 mt-1">
+ 
+                 <label class="block mt-4 text-[#344054] text-sm">Email*</label>
+                 <input type="password" placeholder="Enter your email" class="border border-gray-300 rounded rounded-2xl w-[400px] p-2 mt-1">
+ 
+             
+ 
+                 <div class="mt-4 bg-[#007AFF] w-fit  rounded-2xl ">
+                     <button class="text-white px-16 py-2 w-[400px] rounded rounded-3xl ">Send</button>
+                 </div>
+                 <div class="mt-4  w-fit  rounded-2xl ">
+                     <button class=" px-16 py-2 w-[400px] rounded rounded-3xl border ">Send us a Mail</button>
+                 </div>
+             </form>
+         </div>
+     </section>
+ 
+     
+    </section>
+    <section class="mt-16 py-10 bg-[#0C111D] px-28 flex-col flex justify-center align-center  ">
+         <span class="text-white text-sm text-center align-center flex justify-center">Join 4,000+ companies already growing</span>
+         <div class="flex gap-10 pt-7">
+             <img src="public/images/company1.png" alt="">
+             <img src="public/images/company2.png" alt="">
+             <img src="public/images/company3.png" alt="">
+             <img src="public/images/company4.png" alt="">
+             <img src="public/images/company5.png" alt="">
+             <img src="public/images/company6.png" alt="">
+ 
+         </div>
+ 
+     </section>
+ 
+     <section class="px-28 pt-10">
+         <section>
+             <span class="text-xs text-[#007AFF] font-semibold">Services</span>
+             <h4 class="font-semibold text-[#101828] text-xl mt-2">What we do</h4>
+             <p class="mt-3 text-sm text-[#475467]">
+                 At NotchTec, we are committed to delivering top-notch IT services tailored to <br> business needs. With expertise in software development, IT supplies, and IT <br> consultancy, we empower organizations with cutting-edge technology and <br> innovative solutions.
+ 
+             </p>
+         </section>
+ 
+ 
+         <section class="flex mt-8 gap-24 items-center mb-4 jusitify-center align-center ">
+             <div class="px-4 bg-[#F9FAFB] h-fit pb-4">
+               <div class="mt-2">
+                 <img src="public/images/featuredicon.png" alt="">
+               </div>
+ 
+               <h4 class="mt-6 text-[#101828]">Software Development</h4>
+               <p class="mt-2 text-[#475467]">We specialize in developing high-quality <br> software solutions that enhance efficiency <br> and productivity.</p>
+ 
+               <p class="mt-4 text-[#475467]">Our team creates web and mobile <br> applications, builds enterprise software, and <br> delivers cloud-based applications tailored to <br> meet specific business needs.</p>
+ 
+ 
+               <span class="text-[#007AFF] text-sm mt-2">View integration</span>
+ 
+             </div>
+             
+ 
+             <div class="px-4 bg-[#F9FAFB] h-fit pb-4">
+               <div class="mt-2">
+                 <img src="public/images/featuredicon.png" alt="">
+               </div>
+ 
+               <h4 class="mt-6 text-[#101828]">IT Supplies</h4>
+               <p class="mt-2 text-[#475467]">We provide a comprehensive range of IT <br> hardware and software solutions to support <br> business operations.  Additionally, we supply <br> software licenses, subscriptions, and IT <br> security solutions to ensure systems run <br> smoothly and securely.</p>
+ 
+               <span class="text-[#007AFF] text-sm mt-2">View integration</span>
+ 
+             </div>
+             
+ 
+             <div class="px-4 bg-[#F9FAFB] h-fit pb-4">
+               <div class="mt-2">
+                 <img src="public/images/featuredicon.png" alt="">
+               </div>
+ 
+               <h4 class="mt-6 text-[#101828]">IT Consultancy</h4>
+               <p class="mt-2 text-[#475467]">Our skilled consultants assist businesses in <br> enhancing their IT strategies by providing <br> expert guidance and tailored solutions</p>
+               <span class="text-[#007AFF] text-sm mt-2">View integration</span>
+ 
+             </div>
+ 
+         </section>
+ 
+        
+ 
+     </section>
+     <section class="px-28 py-16 bg-[#F9FAFB] mt-16 flex gap-4 justify-between align-center">
+             <div class=" align-center items-center mt-[200px] ">
+                 <div class="">
+                 <img src="public/images/Stars.png" alt="">
+                 </div>
+             <p class="text-[#101828] text-3xl font-semibold mt-8">
+                 Love the simplicity of the <br> service and the prompt <br> customer support. We <br> can’t imagine working <br> without it.
+             </p>
+             <div class="mt-16">
+ 
+                 <img src="public/images/Attribution.png" alt="">
+             </div>
+             </div>
+ 
+             <div class="">
+                 <img src="public/images/girlimg.png" alt="" class="h-[700px]">
+             </div>
+             
+         </section>
+ 
+         <section class="bg-[#0C111D] flex flex-col justify-center text-white h-[400px]  items-center  ">
+ <div class="flex justify-center items-center mb-6 ">
+     <div class="w-10   ">
+                 <img src="public/images/logomark.png" alt="">
+             </div>
+ </div>
+             
+            
+             <span class="text-xl">Let’s get started on something great</span>
+             <span class="text-xs mt-2">Join over 4,000+ startups already growing with Untitled.</span>
+             <div class="mt-9">
+                 <button class="bg-[#007AFF] text-white rounded-lg px-4 py-2 ">Contact Us</button>
+             </div>
+ 
+ 
+         </section>
+ 
+         <section class="bg-[#0C111D] border-t border-t px-28 text-sm text-white flex justify-between pt-4 py-10 pt-8 items-center border border-l-0 ">
+             <div>
+                <p>© 2077 Untitled UI. All rights reserved.</p>
+ 
+ 
+                 
+             </div>
+             <ul class="flex gap-2 ">
+                     <li>Terms</li>
+                     <li>Privacy</li>
+                     <li>Cookies</li>
+                 </ul>
+ 
+         </section>
+ 
+    
+ </template>
 </template>
